0629e4701b7c1209cf5d911231ee15bf3632563b
Salvador Recuero <salvarecuero@gmail.com>
2021-08-17 15:07:51 -0300

    Now Home pings a convenient endpoint so when Heroku wakes up, it also starts to cache a response for when It's needed.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
   useEffect(() => {
     document.title = "Salvador Recuero";
     // We ping the server so Heroku wakes up it's Dyno.
-    fetch("https://cv-portfolio-server.herokuapp.com/");
+    fetch("https://cv-portfolio-server.herokuapp.com/api/get-tree/");
   }, []);
 
   return (
