aba6248c6648a65f509753fbac066912417799dc
Salvador Recuero <salvarecuero@gmail.com>
2021-09-16 16:16:50 -0300

    Now i do have a job

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,14 +12,10 @@ function AboutMe() {
         <Col span={16}>
           <Text p>
             <i>Web</i> Developer. Enjoying the process step by step. Focused on
-            the frontend and always open to new challenges as required.
-            Currently without job experience, wanting to be part of a great
-            team. I do have so much to learn, but I'm confident that a lot of
-            that knowledge will come along with the experience.
-          </Text>
-          <Text P>
-            Feel free to contact me, even if you don't think I could fit in your
-            team!
+            the frontend and always open to new challenges as required. Wanting
+            to be part of a great team. I do have so much to learn, but I'm
+            confident that a lot of that knowledge will come along with the
+            experience.
           </Text>
         </Col>
       </Row>
